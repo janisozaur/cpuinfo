ed20bf09c10a3afd4ddaa4de1712804568325ef3
Michał Janiszewski <janisozaur@users.noreply.github.com>
2023-05-05 15:15:10 +0200

    Update test.c

--- src/test.c
+++ src/test.c
@@ -2,8 +2,12 @@
 #include <cpuinfo.h>
 
 int main() {
+  printf("Init\n");
+  fflush(stdout);
   cpuinfo_initialize();
   printf("Running on %s CPU\n", cpuinfo_get_package(0)->name);
   cpuinfo_deinitialize();
+  printf("Denit\n");
+  fflush(stdout);
   return 0;
 }
